8fbfbfdfcf78a7acfd54483a532f7370de3c8019
jdagmar <dagmar.j.gustavsson@gmail.com>
2018-03-05 14:45:52 +0100

    search results now shows in complete list.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const input = document.getElementById('input');
 const searchForm = document.getElementById('search-form');
+const drinkContainer = document.getElementById('drink-div');
+const drinkList = document.getElementById('drink-list');
+
+const randomDrink = document.getElementById('random-drink');
+const searchResultHeader = document.getElementById('search-result-header');
 
 const refreshButton = document.getElementById('refresh-button');
 
@@ -9,34 +14,47 @@ const searchForDrink = (searchWord) => {
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
-            displayDrink(data.drinks);
 
+            // let drinkId = [];
+
+            // for (drink of drinks) {
+            //     drinkId += drink;
+            // }
+
+            // searchForDrinkIngredients(drinkId);
+
+            displayDrink(data.drinks, 'list');
         })
         .catch((error) => {
-            console.log('error', error)
+            console.log('sorry no result');
         });
 }
 
-searchForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const searchValue = input.value;
-    searchForDrink(searchValue);
-})
+// const searchForDrinkIngredients = (id) => {
+//     fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+//         .then((response) => response.json())
+//         .then((data) => {
+
+//         })
+//         .catch((error) => {
+//             console.log('error', error)
+//         });
+// }
 
 const getRandomDrink = () => {
     fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
-            displayDrink(data.drinks);
+            displayDrink(data.drinks, 'single');
 
         })
         .catch((error) => {
-            console.log('error', error)
+            console.log('couldnt find any recipe');
         });
 }
 
-const displayDrink = (drinks) => {
+const displayDrink = (drinks, type) => {
 
     for (const drink of drinks) {
 
@@ -44,62 +62,88 @@ const displayDrink = (drinks) => {
         const drinkImageContainer = document.getElementById('drink-image-container');
         const drinkIngredientsContainer = document.getElementById('drink-ingredients');
 
-        let drinkInfo = `
+        // display when random drink
+        if (type === 'single') {
+            let drinkInfo = `
                 <p>${drink.strDrink}</p>
             `;
 
-        let drinkImage = `
+            let drinkImage = `
             <img src="${drink.strDrinkThumb}" alt="${drink.strDrink}"/>
         `;
 
-        let measureArray = [];
-        let ingredientsArray = [];
-        let drinksProperties = Object.keys(drink);
+            let measureArray = [];
+            let ingredientsArray = [];
+            let drinksProperties = Object.keys(drink);
 
-        for (const drinkProperty of drinksProperties) {
-            if (drinkProperty.includes('strIngredient')) {
+            for (const drinkProperty of drinksProperties) {
+                if (drinkProperty.includes('strIngredient')) {
 
-                if (drink[drinkProperty] !== null) {
-                    ingredientsArray.push(drink[drinkProperty]);
+                    if (drink[drinkProperty] !== null) {
+                        ingredientsArray.push(drink[drinkProperty]);
+                    }
+                }
+
+                if (drinkProperty.includes('strMeasure')) {
+                    measureArray.push(drink[drinkProperty]);
                 }
             }
 
-            if (drinkProperty.includes('strMeasure')) {
-                measureArray.push(drink[drinkProperty]);
-            }
-        }
+            let drinkIngredients = '';
 
-        let drinkIngredients = '';
+            for (let i = 0; i < ingredientsArray.length; i++) {
 
-        for (let i = 0; i < ingredientsArray.length; i++) {
+                const ingredient = ingredientsArray[i];
+                const measures = measureArray[i];
 
-            const ingredient = ingredientsArray[i];
-            const measures = measureArray[i];
-
-            drinkIngredients += `
+                drinkIngredients += `
                 <li class="list-reset mb-2">${measures} ${ingredient}</li>
             `;
 
-        }
+            }
 
-        const drinkInstructionsContainer = document.getElementById('drink-instructions');
+            const drinkInstructionsContainer = document.getElementById('drink-instructions');
 
-        let drinkInstructions = `
+            let drinkInstructions = `
                 <p>${drink.strInstructions}</p>
             `;
 
-        drinkTitle.innerHTML = drinkInfo;
-        drinkImageContainer.innerHTML = drinkImage;
-        drinkIngredientsContainer.innerHTML = drinkIngredients;
-        drinkInstructionsContainer.innerHTML = drinkInstructions;
+            drinkTitle.innerHTML = drinkInfo;
+            drinkImageContainer.innerHTML = drinkImage;
+            drinkIngredientsContainer.innerHTML = drinkIngredients;
+            drinkInstructionsContainer.innerHTML = drinkInstructions;
+        }
+
+        // display when search
+        if (type === 'list') {
+
+            randomDrink.style.display = 'none';
+        
+            searchResultHeader.innerText = `Search result(s) for ${input.value}:`;
+
+            const searchResult = drinkContainer.cloneNode(true);
+
+            const searchResultTitle = searchResult.querySelector('.drink-title');
+            searchResultTitle.innerText = drink.strDrink;
+
+            let drinkImageList = `<img src="${drink.strDrinkThumb}" alt="${drink.strDrink}"/>`;
+
+            drinkImageContainer.innerHTML = drinkImageList;
+
+            drinkList.appendChild(searchResult);
+
+        }
+
     }
 
 }
 
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const searchValue = input.value;
+    searchForDrink(searchValue);
+});
+
 refreshButton.addEventListener('click', () => {
     getRandomDrink();
-})
-
-
-
-
+});
